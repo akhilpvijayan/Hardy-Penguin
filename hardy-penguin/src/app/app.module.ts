30a6890916381c14e449448bdc77c2a9466557a5
Akhil <akhil.vijayan@experionglobal.com>
2023-12-27 00:00:47 +0530

    Added firebase

--- hardy-penguin/src/app/app.module.ts
+++ hardy-penguin/src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDJq8-G4NgQUgA9CnOD-pgQWdzNKhgN-fo",
+      authDomain: "hardypenguin-25b26.firebaseapp.com",
+      projectId: "hardypenguin-25b26",
+      storageBucket: "hardypenguin-25b26.appspot.com",
+      messagingSenderId: "381177177842",
+      appId: "1:381177177842:web:d3993f996931f4f0a1526b"
+    }),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
